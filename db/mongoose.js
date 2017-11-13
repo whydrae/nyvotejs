f500062076314e8d79305cad377e1368c196d3f8
whydrae <eliasklopkov@Eliass-MacBook-Pro.local>
2017-11-14 00:19:50 +0300

    working on things

---
+++ db/mongoose.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var config = require('../config.js');
+
+mongoose.connect(config.get('mongoose:uri'), {
+  useMongoClient: true
+});
+
+var db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('Connection error ' + err.message);
+});
+
+db.once('open', function callback() {
+  console.info("Connected to DB!");
+});
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+var config = require('./config');
 
-var port = process.env.PORT || 8080;
+var db = require('./db/mongoose');
+
+var port = process.env.PORT || config.get('port');
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+// passport
+app.use(session({
+  secret: config.get('session:secret')
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(__dirname + 'public'));
 
