353f984a90a6de2e58d5daa57708268d260950ff
whydrae <eklopkov@gmail.com>
2017-11-15 16:31:35 +0300

    pushing to the limits

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,8 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
     username: String,
-    password: String
+    password: String,
+    couple: { type: Schema.Types.ObjectId, ref: 'users' }
 });
 
 User.plugin(passportLocalMongoose);
--- app/routes/santas.js
+++ app/routes/santas.js
@@ -5,11 +5,11 @@ var passport = require('passport');
 var Santa = require('../models/santa');
 var User = require('../models/user');
 
-router.post('/recipient', function(req, res) {
+router.post('/recipient', function (req, res) {
   if (req.isAuthenticated()) {
     Santa.findOne({
       'from': req.user._id
-    }, function(err, santa) {
+    }, function (err, santa) {
       if (err) {
         return res.status(500).json({
           err: err
@@ -18,17 +18,17 @@ router.post('/recipient', function(req, res) {
       if (santa) {
         res.json(santa);
       } else {
-        User.find(function(err, users) {
+        this.getRandomUserForSanta(req.user, function (err, user) {
           if (err) {
             return res.status(500).json({
               err: err
             });
           }
-          if (users) {
+          if (user) {
             Santa.create({
               from: req.user._id,
-              to: users[1]._id
-            }, function(err, recipient) {
+              to: user._id
+            }, function (err, recipient) {
               if (err) {
                 return res.status(500).json({
                   err: err
@@ -42,9 +42,55 @@ router.post('/recipient', function(req, res) {
             });
           }
         });
+        // User.find(function (err, users) {
+        //   if (err) {
+        //     return res.status(500).json({
+        //       err: err
+        //     });
+        //   }
+        //   if (users) {
+        //     Santa.create({
+        //       from: req.user._id,
+        //       to: users[1]._id
+        //     }, function (err, recipient) {
+        //       if (err) {
+        //         return res.status(500).json({
+        //           err: err
+        //         });
+        //       }
+        //       if (recipient) {
+        //         return res.status(200).json({
+        //           recipient: recipient
+        //         });
+        //       }
+        //     });
+        //   }
+        // });
       }
     });
   }
 });
+
+function getRandomUserForSanta(satna, callback) {
+  User.find({
+    _id: {
+      $ne: santa._id
+    }
+  }, function (err, users) {
+    if (err) {
+      callback(err);
+    }
+    if (users) {
+      if (users.length > 1) {
+        do {
+          var user = items[Math.floor(Math.random() * users.length)];
+        } while (user.couple !== santa._id);
+      } else {
+        user = users[0];
+      }
+      callback({}, user);
+    }
+  });
+}
 
 module.exports = router;
