7d0437b9924509883c0b64f5cd944804efb3538a
whydrae <eklopkov@gmail.com>
2018-11-26 18:10:48 +0300

    fixing santas route

--- app/routes/santas.js
+++ app/routes/santas.js
@@ -5,7 +5,7 @@ const Santa = require('../models/santa');
 const User = require('../models/user');
 const Wish = require('../models/wish');
 
-router.get('/recipient', function(req, res) {
+router.get('/recipient', function (req, res) {
   if (!req.isAuthenticated()) {
     return res.status(401).json({
       err: "Unauthorized"
@@ -13,36 +13,33 @@ router.get('/recipient', function(req, res) {
   }
 
   Santa.findOne({
-    'from': req.user._id
-  }, function(err, santa) {
-    if (err) {
-      return res.status(500).json({
-        err: err
-      });
-    }
-    if (santa) {
-      User.findOne({
-          _id: santa.to
-        },
-        function(err, user) {
-          if (err) {
-            return res.status(500).json({
-              err: err
-            });
-          }
-          return res.status(200).json({
-            recipient: user
-          });
-        });
-    } else {
-      return res.status(200).json({
-        err: "You're not a santa!"
-      });
-    }
-  });
+      from: req.user.id
+    })
+    .then((santa) => {
+      if (santa) {
+        User.findOne({
+            _id: santa.to
+          })
+          .then((user) => {
+            res.status(200).json({
+              recipient: user
+            })
+          })
+          .catch((err) => res.status(500).json({
+            err: err
+          }))
+      } else {
+        return res.status(200).json({
+          err: "You're not a santa!"
+        })
+      }
+    })
+    .catch((err) => res.status(500).json({
+      err: err
+    }))
 });
 
-router.post('/recipient', function(req, res) {
+router.post('/recipient', function (req, res) {
   if (!req.isAuthenticated()) {
     return res.status(401).json({
       err: "Unauthorized"
@@ -58,7 +55,7 @@ router.post('/recipient', function(req, res) {
           recipient: santa
         });
       } else {
-        getRandomUserForSanta(req.user, function(err, user) {
+        getRandomUserForSanta(req.user, function (err, user) {
           if (err) {
             return res.status(500).json({
               err: err
@@ -94,7 +91,7 @@ router.post('/recipient', function(req, res) {
     }))
 });
 
-router.post('/reset', function(req, res) {
+router.post('/reset', function (req, res) {
   if (!req.isAuthenticated()) {
     return res.status(401).json({
       err: "Unauthorized"
@@ -111,19 +108,19 @@ router.post('/reset', function(req, res) {
     }))
 });
 
-router.post('/check', function(req, res) {
+router.post('/check', function (req, res) {
   if (!req.isAuthenticated()) {
     return res.status(401).json({
       err: "Unauthorized"
     });
   }
 
-  Santa.find({}, function(err, santas) {
+  Santa.find({}, function (err, santas) {
     for (var i = 0; i < santas.length; i++) {
       var santa = santas[i];
       User.findById({
         _id: santa.from
-      }, function(err, user) {
+      }, function (err, user) {
         if (user.couple.equals(santa.to)) {
           return res.status(500).json({
             status: "Couple found!"
@@ -138,19 +135,13 @@ router.post('/check', function(req, res) {
 });
 
 function getRandomUserForSanta(santa, callback) {
-  Santa.find({}, function(err, santas) {
+  Santa.find({}, function (err, santas) {
     // searching for users than already have santa
     var haveSanta = [];
     if (santas) {
-      haveSanta = santas.map(function(santaFound) {
+      haveSanta = santas.map(function (santaFound) {
         return santaFound.to;
       });
-      // santa <> from
-      // santas.forEach(function(santaEach) {
-      //   if (santaEach.to.equals(santa._id)) {
-      //     haveSanta.push(santaEach.from);
-      //   }
-      // });
     }
 
     User.find({
@@ -166,51 +157,49 @@ function getRandomUserForSanta(santa, callback) {
           }
         ]
       },
-      function(err, users) {
+      function (err, users) {
         if (err) {
           return callback(err);
         }
         if (users) {
           var user = null;
 
-          if (users.length > 1) {
-            // last user has no santa, nor recipient
-            if (users.length === 2) {
-              for (var i = 0; i < users.length; i++) {
-                var userLast = users[i];
-                let found = false;
+          // there's only one user left
+          if (users.length === 1) {
+            callback(null, user);
+            return;
+          }
 
-                for (var j = 0; j < santas.length; j++) {
-                  if (santas[j].from.equals(userLast._id)) {
-                    found = true;
-                  }
-                }
+          // last user has no santa, nor recipient
+          if (users.length === 2) {
+            let found = false;
 
-                if (found === false) {
-                  user = userLast;
+            for (var i = 0; i < users.length; i++) {
+              var lastUser = users[i];
+
+              for (var j = 0; j < santas.length; j++) {
+                if (santas[j].from.equals(lastUser._id)) {
+                  found = true;
                 }
               }
             }
 
-            if (user === null) {
-              let i = 0;
-              let found = false;
-              // magic
-              do {
-                i = i + 1;
-                user = users[Math.floor(Math.random() * users.length)];
-                if (user.couple.equals(santa._id)) {
-                  continue;
-                } else {
-                  found = true;
-                  break;
-                }
-              } while (!found || i > 500);
+            if (!found) {
+              callback(null, lastUser);
+              return;
             }
-            // end magic
-          } else {
-            user = users[0];
           }
+
+          // get random
+          let stopNumber = 0;
+          do {
+            stopNumber = stopNumber + 1;
+            user = users[Math.floor(Math.random() * users.length)];
+            if (user.couple.equals(santa._id)) {
+              continue;
+            }
+            break;
+          } while (stopNumber > 500);
 
           callback(null, user);
         }
